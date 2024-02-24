bebb0b4a3474a5b3e76d511c25d1df6a39e922d9
incognitotgt <mashoor539@icloud.com>
2024-02-24 01:02:30 -0500

    reinit shit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="astro/client" />
-interface Window {
-  navigator: {
-  keyboard: {
-    lock: (keys: string[]) => void;
-  };
-};
-  __uv$config: {
-    encodeUrl: (url: string) => string;
-  };
-}
